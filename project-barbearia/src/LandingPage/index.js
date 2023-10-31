f1ad568b3240122af8a207d0962dbd4a6f991a02
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2023-10-31 16:33:53 -0300

    Responsivo concluído

--- project-barbearia/src/LandingPage/index.js
+++ project-barbearia/src/LandingPage/index.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import {useState} from 'react';
 import './style.css';
 
 export default function LandingPage(){
@@ -21,27 +21,28 @@ export default function LandingPage(){
             <header className={estiloAtualizado}>
                 <div className='secao-limitada secao-topo'>
                     <img className='img-logo' src="assets/barbearia-logo.png" alt="logo"/>
-                    <button onClick={mudarTema} className={estiloAtualizado == corDeFundoEscuro ? 'tema-escuro' : 'tema-claro'}>
-                        <img className='img' src={estiloAtualizado == corDeFundoEscuro ? 'assets/sun.png' : 'assets/moon.png'} alt="logo"/>
-                        {estiloAtualizado == corDeFundoEscuro ? 'Light' : 'Dark'}
+                    <button onClick={mudarTema} className={estiloAtualizado === corDeFundoEscuro ? 'tema-escuro' : 'tema-claro'}>
+                        <img className='img' src={estiloAtualizado === corDeFundoEscuro ? 'assets/sun.png' : 'assets/moon.png'} alt="logo"/>
+                        {estiloAtualizado === corDeFundoEscuro ? 'Light' : 'Dark'}
                     </button>
                 </div>
             </header>
             <main className={estiloAtualizado}>
                 <div className='img-banner'>
                 </div>
-                <div className='secao-limitada conteudo'>
-                    <h1>Bem-vindo a Barber Shop</h1>
-                    <p>
-                        Nossa barbearia sempre oferece profissionais de qualidade e estamos prontos para lidar com suas maiores expectativas.
-                    </p>
-                    <p>
-                        Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos uma equipe premiada que demonstrou o talento de mestres barbeiros em vários concursos de estilo. Deixe nosso barbeiro ser seu estilista pessoal e você nunca ficará desapontado.
-                    </p>
-
-                    <p>
-                        S. Kelly
-                    </p>
+                <div className='secao-conteudo'>
+                    <div className='secao-limitada conteudo'>
+                        <h1>Bem-vindo a Barber Shop</h1>
+                        <p className='primeiro-paragrafo'>
+                            Nossa barbearia sempre oferece profissionais de qualidade e estamos prontos para lidar com suas maiores expectativas.
+                        </p>
+                        <p>
+                            Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos uma equipe premiada que demonstrou o talento de mestres barbeiros em vários concursos de estilo. Deixe nosso barbeiro ser seu estilista pessoal e você nunca ficará desapontado.
+                        </p>
+                        <p className='assinatura'>
+                            S. Kelly
+                        </p>
+                    </div>
                 </div>
             </main>
         </div>
